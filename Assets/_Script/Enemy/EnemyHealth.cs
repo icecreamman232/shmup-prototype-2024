dac6d9cb101b7274cd58a934c51318245a5d4b16
Stephen Ho <stephen.ho.232@gmail.com>
2024-05-01 01:36:52 +0700

    Add enemy death sfx

--- Assets/_Script/Enemy/EnemyHealth.cs
+++ Assets/_Script/Enemy/EnemyHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using JustGame.Script.Manager;
 using JustGame.Scripts.Common;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 
 public class EnemyHealth : Health
@@ -8,7 +9,7 @@ public class EnemyHealth : Health
     [SerializeField] private EnemyHealthBar m_healthBar;
     [SerializeField] private SpriteRenderer m_spriteRenderer;
     [SerializeField] private AnimationParameter m_deathAnim;
-    
+    [SerializeField] private MMF_Player m_deathSFX;
     protected override void UpdateHealthUI()
     {
         m_healthBar.UpdateHealthBar(MathHelpers.Remap(m_curHealth,0,m_maxHealth,0,1));
@@ -30,6 +31,7 @@ public class EnemyHealth : Health
         m_isInvulnerable = true;
         m_spriteRenderer.enabled = false;
         m_deathAnim.SetTrigger();
+        m_deathSFX.PlayFeedbacks();
         yield return new WaitForSeconds(m_deathAnim.Duration);
         base.Kill();
     }
